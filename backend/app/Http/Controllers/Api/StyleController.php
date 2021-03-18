a259638e7733ceda5fad3f17d0d51b32936948c8
Ho Thi Ma1 <mai1.ho21@pn.com.vn>
2021-03-18 15:31:16 +0700

    Just add role table

--- backend/app/Http/Controllers/Api/StyleController.php
+++ backend/app/Http/Controllers/Api/StyleController.php
@@ -10,39 +10,37 @@ class StyleController extends Controller
   // Show all the Styles
   public function show()
   {
-      return response()->json(Style::all(),200); 
+    return response()->json(Style::all(),200); 
   }
 
 // add new style
   public function store(Request $request)
   {
-      $styles = Style::create([
-          'image' => $request->image,
-          'name' => $request->name,
-          'price' => $request->price,
-              ]);
+    $styles = Style::create([
+      'image' => $request->image,
+      'name' => $request->name,
+      'price' => $request->price,
+    ]);
       return response()->json(['data'=>$styles]);
   }
 
 // update the Style
   public function update(Request $request, Style $styles)
   {
-      $update_style = $styles->update(
-          $request->only(['image', 'name', 'price'])
-      );
-
-      return response()->json([
-          'update_style' => $update_style
-      ]);
+    $update_style = $styles->update(
+      $request->only(['image', 'name', 'price'])
+    );
+    return response()->json([
+      'update_style' => $update_style
+    ]);
   }
 
 // deletes an Style.
   public function destroy(Style $style)
   {
-      $status = $style->delete();
-
-      return response()->json([
-          'status' => $status
-      ]);
+    $status = $style->delete();
+    return response()->json([
+      'status' => $status
+    ]);
   }
 }
