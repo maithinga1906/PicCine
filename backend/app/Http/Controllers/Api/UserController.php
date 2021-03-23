af7e6c954ac17beeddb9671fda42ff7255c288d4
PN\thuat.ha21 <havuthuatdev@gmail.com>
2021-03-23 16:01:37 +0700

    update Controller.

--- backend/app/Http/Controllers/Api/UserController.php
+++ backend/app/Http/Controllers/Api/UserController.php
@@ -4,16 +4,36 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Http\Requests\UserRequest;
-
-
+use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\LoginRequest;
+use App\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 class UserController extends Controller
 {
-    //
+    public function register(RegisterRequest $request) {
+        $user = new User;
+        $user->fill($request->all());
+        $user->password = Hash::make($request->password);
+        $user->save();
 
-    public function register(Request $request) {
-        return 1;
+        return response()->json($user);
+    }
+    public function login(LoginRequest $request) {
+        if(Auth::attempt([
+            'email' => $request->email,
+            'password' => $request->password
+        ])){
+            $user = User::whereEmail($request->email)->first();
+            $user->token = $user->createToken('App')->accessToken;
 
+            return response()->json($user);
+        }
+        return response()->json(['email' => 'sai ten truy cap hoac mat khau'], 401);
+    }
+
+    public function userInfo(Request $request) {
+        return response()->json($request->user('api')); 
     }
 
     
