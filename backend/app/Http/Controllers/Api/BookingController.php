892e0c68a9c5edae381816d54428d48748544e78
dangvannhang <dangvannhang107@gmail.com>
2021-03-24 09:49:46 +0700

    Delete detailpost, detailbooking migrations and models.

--- backend/app/Http/Controllers/Api/BookingController.php
+++ backend/app/Http/Controllers/Api/BookingController.php
@@ -14,28 +14,7 @@ class BookingController extends Controller
 //schedule a photoshoot
     public function booking(Request $request)
     {   
-        $booking= new Booking;
-        // $booking->fill($request->all());
-        $booking->id_user=$request->id_user;
-        $booking->id_photographer=$request->id_photographer;
-        $booking->id_combo=$request->id_combo;
-        $booking->is_cancel=$request->is_cancel;
-        $booking->save();
-        
 
-        $detailBooking= new DetailBooking;
-        $detailBooking->id_booking=$request->id_booking;
-        $detailBooking->address= $request->address;
-        $detailBooking->user_phone=$request->user_phone;
-        $detailBooking->photographer_phone=$request->photographer_phone;
-        $detailBooking->user_name=$request->user_name;
-        $detailBooking->photographer_name=$request->photographer_name;
-        $detailBooking->code_voucher=$request->code_voucher;
-        $detailBooking->name_style=$request->name_style;
-        $detailBooking->save();
-
-        //  khong the tra ve $booking va detailBooking cung 1 luc duoc
-        return response()->json($detailBooking);
 
     }
 }
