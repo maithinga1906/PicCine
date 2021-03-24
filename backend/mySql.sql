09ccf0b879f55f192100808242e3e1b264ebb93e
dangvannhang <dangvannhang107@gmail.com>
2021-03-24 09:39:55 +0700

    Edit database by delete detail booking.

--- backend/mySql.sql
+++ backend/mySql.sql
@@ -50,6 +50,7 @@ create table booking(
     created_time datetime,
     id_combo int,
     is_cancel boolean,
+    code_voucher varchar(50),
     primary key(id),
     foreign key(id_user) references users(id),
     foreign key(id_photographer) references users(id),
@@ -57,43 +58,16 @@ create table booking(
     
 );
 
-create table detail_booking(
-	id int auto_increment,
-    id_booking int,
-    address varchar(255),
-    user_phone numeric,
-    photographer_phone numeric,
-    user_name varchar(50), 
-    photographer_name varchar(50),
-    code_vourcher varchar(50),
-    name_style varchar(50),
-    primary key(id),
-    foreign key(id_booking) references booking(id)
-    -- foreign key(name_style) references combo(name_style) 
-    -- foreign key(code_vourcher) references voucher(codee) 
-    
-);
 
--- create table studio(
--- 	id int auto_increment,
---     address varchar(255),
---     phone numeric
--- );
-
-create table post(
+create table posts(
 	id int auto_increment,
-    id_photographer int,
-    primary key(id),
-    foreign key(id_photographer) references users(id)
-);
-create table post_detail(
-	id int auto_increment,
+	id_photographer int,
     title varchar(50),
     content varchar(255),
     img varchar(255),
 	id_post int,
-    foreign key(id_post) references post(id),
-    primary key(id)
+    primary key(id),
+	foreign key(id_photographer) references users(id)
 );
 
 
