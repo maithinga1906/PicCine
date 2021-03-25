3607a663b531f864ae5a9a176c7773d2e66df9d6
dangvannhang <dangvannhang107@gmail.com>
2021-03-25 16:49:37 +0700

    Some method about booking schedule.

--- backend/app/Http/Controllers/Api/BookingController.php
+++ backend/app/Http/Controllers/Api/BookingController.php
@@ -14,11 +14,13 @@ class BookingController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+
+    // Hiển thị tất cả các lịch booking từ mới tới cũ, giới hạn show ra theo page, mỗi page chỉ có 5 booking schedule
     public function index()
     {
         //
-        $booking = new Booking;
-        $booking = Booking::all();
+        // $booking = new Booking;
+        $booking = Booking::latest()->get();
 
         return response()->json($booking);
     }
@@ -52,6 +54,7 @@ class BookingController extends Controller
         return response()->json($booking);
     }
 
+
     /**
      * Update the specified resource in storage.
      *
@@ -59,22 +62,22 @@ class BookingController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
+    // update dùng ở đây để người dùng có thể thay đổi combo hay thời gian đặt vậy đó.
     public function update(Request $request, $id)
     {
         //
         $booking=Booking::findOrFail($id);
-
-
         $booking->is_cancel='false';
         $booking->code_voucher="Editbooking122";
         $booking->save();
-
-
         // hiện đang không thể $request tới các trường ở input, nên phải fix data để tét
         // $booking->save();
 
         return response()->json($booking);
     }
+
+
+
 
     /**
      * Remove the specified resource from storage.
@@ -90,4 +93,22 @@ class BookingController extends Controller
 
         return response()->json("Delete finished");
     }
+
+
+    //  Khi mà người dùng không muốn đặt lịch book nữa thì họ có thể cancel nhưng cái lịch booking vẫn lưu lại, chỉ chuyển is_cancel thành false
+    public function cancel_booking($id, Request $request) {
+        $booking=Booking::find($id);
+
+        $booking->is_cancel="true";
+        $booking->save();
+
+        return response()->json($booking);
+    }
+
+    // Hiển thị các booking schedule đã bị hủy
+    public function show_cancel_booking() {
+        $booking = Booking::where('is_cancel','true')->get();
+        return response()->json($booking);
+    }
+
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -36,9 +36,9 @@ Route::get('booking/index','Api\BookingController@index')->middleware('auth:api'
 Route::get('/booking/show/{id}','Api\BookingController@show')->middleware('auth:api');
 Route::post('/booking/store', 'Api\BookingController@store')->middleware('auth:api');
 Route::patch('/booking/update/{id}','Api\BookingController@update')->middleware('auth:api');
-Route::delete('booking/destroy/{id}','Api\BookingController@destroy');
-
-
+Route::delete('booking/destroy/{id}','Api\BookingController@destroy')->middleware('auth:api');
+Route::get('/booking/cancel-booking/{id}','Api\BookingController@cancel_booking')->middleware('auth:api');
+Route::get('/booking/show-cancel-booking','Api\BookingController@show_cancel_booking')->middleware('auth:api');
 
 
 
